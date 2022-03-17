2dd1fd2e88a2941017726ddd2f2ab9fbe3e637ef
Jeremie Delaitre <jeremie.delaitre@gmail.com>
2022-03-17 19:29:39 +1300

    highlight each capture individually, or the whole match if no capture

--- src/rule_highlighter.cpp
+++ src/rule_highlighter.cpp
@@ -29,7 +29,12 @@ void rule_highlighter_t::highlightBlock(const QString& text)
         while (it.hasNext())
         {
             QRegularExpressionMatch match = it.next();
-            setFormat(match.capturedStart(), match.capturedLength(), class_format);
+
+            // Highlight each capture individually, or if no specific capture group was specified
+            // highlight the whole match.
+            int start_index = (match.lastCapturedIndex() == 0) ? 0 : 1;
+            for (int i = start_index; i <= match.lastCapturedIndex(); ++i)
+                setFormat(match.capturedStart(i), match.capturedLength(i), class_format);
         }
     }
 }
